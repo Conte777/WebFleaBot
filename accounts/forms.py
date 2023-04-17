6192106715dab713a8476b40d24a7c360d08c8f0
Conte <alexey.kolesnikov77@gmail.com>
2023-04-17 22:47:46 +0300

    Удаление мусора

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,18 +1,12 @@
 from .models import UserModel
 from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
 from django.contrib.auth.forms import UserChangeForm as BaseUserChangeForm
-# from send_request.models import SendModel
 
 
 class UserCreationForm(BaseUserCreationForm):
     class Meta(BaseUserCreationForm.Meta):
         model = UserModel
         fields = ("username", "email")
-
-    # def save(self, commit=True):
-    #     user = super().save()
-    #     user.create_sendmodels()
-    #     return user
 
 
 class UserChangeForm(BaseUserChangeForm):
--- accounts/models.py
+++ accounts/models.py
@@ -5,14 +5,6 @@ from django.contrib.auth.models import UserManager
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.core.mail import send_mail
-from send_request.models import SendModel
-
-
-# class UserManager(BaseUserManager):
-#     def _create_user(self, username, email, password, **extra_fields):
-#         user = super()._create_user(username, email, password, **extra_fields)
-#         user.create_sendmodels()
-#         return user
 
 
 class UserModel(AbstractBaseUser, PermissionsMixin):
@@ -66,8 +58,3 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
     def email_user(self, subject, message, from_email=None, **kwargs):
         """Send an email to this user."""
         send_mail(subject, message, from_email, [self.email], **kwargs)
-
-    # def create_sendmodels(self):
-    #     for i in range(1, self.number_sending_models + 1):
-    #         SendModel.objects.create(
-    #             user=self, form_name=str(i))
